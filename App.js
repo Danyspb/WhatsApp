c44deb8b6503ed8a7812272d9f7f5482e46a56fd
Danyspb4ever <danyspb4ever@gmail.com>
2022-12-02 21:09:20 +0000

    useEffect

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
+
+  useEffect(()=> {
+    console.log('ca marche')
+  },[count, newCount])
+
+  const add = ()=>{
+    setCount(plus => plus +1)
+    if(count > 9){
+      setCount(10)
+    }
+  }
+  const dimu = ()=>{
+    setNewCount(newCount -1)
+    if(count < 1){
+      setCount(0)
+    }
+  }
+ 
+   
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+       <Button title='Touche pas +' onPress={add} />
+        <Text style={styles.text}>{count}</Text>
+        <Button title='Click Me -' onPress={dimu} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +42,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  text:{
+    fontSize: 25,
+    marginTop: 5,
+    marginBottom: 5,
+    alignSelf: 'center'
+  }
+
 });
+ 
