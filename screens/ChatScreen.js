33317d54e6833865a5b1295d6c60f694f861d77c
Danyspb4ever <danyspb4ever@gmail.com>
2022-12-07 14:58:40 +0000

    add button and test input

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { Text, View, StyleSheet, Button, ImageBackground } from "react-native";
-import backgroundImage from "../assets/images/WallpaperDog-5554827.jpg"
+import { Text, View, StyleSheet, Button, ImageBackground, TextInput } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
 
 const ChatScreen = (props) =>{
     return (
-        <View style={styles.container}>
-            <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
-
-            </ImageBackground>
-        </View>
+        
+        <SafeAreaView style={styles.container} edges={['right','bottom','bottom']}>
+            <ImageBackground source={require('../assets/images/WallpaperDog-5554827.jpg')} style={styles.backgroundImage} />
+            <View style={styles.inputContainer}>
+                <Button title="Image" /> 
+                <TextInput />
+                <Button title="Camera" /> 
+            </View>
+        </SafeAreaView>
+      
     )
 }
 const styles = StyleSheet.create({
     container:{
         flex:1,
         flexDirection: 'column',
+        resizeMode: 'cover'
     },
     backgroundImage:{
         flex: 1
+    },
+    inputContainer:{
+        flexDirection: 'row',
+        paddingVertical: 8,
+        paddingHorizontal: 10,
+        height: 50,
+
+
     }
 })
 
