973271829a1965e9a0492cc4245065a3ad5577dc
Danyspb4ever <danyspb4ever@gmail.com>
2022-12-07 15:25:10 +0000

    add button icon

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Text, View, StyleSheet, Button, ImageBackground, TextInput } from "react-native";
+import { Text, View, StyleSheet, Button, ImageBackground, TextInput, TouchableOpacity } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const ChatScreen = (props) =>{
@@ -9,9 +11,13 @@ const ChatScreen = (props) =>{
         <SafeAreaView style={styles.container} edges={['right','bottom','bottom']}>
             <ImageBackground source={require('../assets/images/WallpaperDog-5554827.jpg')} style={styles.backgroundImage} />
             <View style={styles.inputContainer}>
-                <Button title="Image" /> 
+                <TouchableOpacity onPress={()=> console.log("#0002A1")}>
+                <Feather name="plus" size={24} color="#3498db" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=> console.log("press")}>
+                <Ionicons name="camera" size={24} color="#0002A1" />
+                </TouchableOpacity>
                 <TextInput />
-                <Button title="Camera" /> 
             </View>
         </SafeAreaView>
       
@@ -31,8 +37,6 @@ const styles = StyleSheet.create({
         paddingVertical: 8,
         paddingHorizontal: 10,
         height: 50,
-
-
     }
 })
 
